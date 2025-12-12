8d8bf72a61b7b2238518faf8fc80ddedbb8f5fd3
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2025-12-12 11:37:17 +0100

    add more readable day 11 solution

--- day-11-unwatched-gifts/solution.ts
+++ day-11-unwatched-gifts/solution.ts
@@ -1,41 +1,63 @@
 /* 
-I don't like the word 'unsafe' it has some bad connotations.
+The word 'unsafe' has  bad connotations.
 
 solution:
 - Find gift index in string,
 - Check at most 4 other locations, left, right, up, down
 - if none of them contain camera, add +1 
 */
-export function findUnsafeGifts(warehouse: string[]): number {
-    let result = 0;
-    warehouse.forEach((line, outerIndex) => {
-        Array.from(line).forEach((character, index) => {
-            // ignore lines that don't contain gits
+export const findUnsafeGifts = (warehouse: string[]): number => {
+    const hasCamera = ({ row, column }) => {
+        const board = warehouse;
+        const height = board.length;
+        const width = height > 0 ? board[0].length : 0;
+
+        // out of board bounds
+        if (row < 0 || row >= height) {
+            return false;
+        }
+
+        if (column < 0 || column >= width) {
+            return false;
+        }
+
+        const element = board[row][column];
+
+        return element === "#";
+    };
+
+    const findGifts = (input: string) => {
+        const array: Array<number> = [];
+
+        for (let index = 0; index < input.length; ++index) {
+            const character = input[index];
             if (character !== "*") {
-                return;
+                continue;
             }
 
-            // check left and right if possible
-            if (index > 0 && line[index - 1] === "#") {
-                return;
-            }
-            if (index < line.length - 1 && line[index + 1] === "#") {
-                return;
-            }
+            array.push(index);
+        }
 
-            // check up and down if possible
-            if (outerIndex > 0 && warehouse[outerIndex - 1][index] === "#") {
-                return;
-            }
-            if (
-                outerIndex < warehouse.length - 1 &&
-                warehouse[outerIndex + 1][index] === "#"
-            ) {
-                return;
-            }
+        return array;
+    };
 
-            result += 1;
-        });
-    });
+    let result = 0;
+
+    for (let row = 0; row < warehouse.length; ++row) {
+        const line = warehouse[row];
+        for (const column of findGifts(line)) {
+            // left, right, up, down
+            const hasAnyCamera =
+                hasCamera({ row, column: column - 1 }) ||
+                hasCamera({ row, column: column + 1 }) ||
+                hasCamera({ row: row - 1, column }) ||
+                hasCamera({ row: row + 1, column });
+
+            if (!hasAnyCamera) {
+                result += 1;
+            }
+        }
+    }
+
     return result;
-}
+};
