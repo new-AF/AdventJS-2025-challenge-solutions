84ef710aa497ac4a66293ab5d314d82778113340
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2025-12-13 13:14:37 +0100

    add day 13 initial testbed

---
+++ day-13-assembly-line/solution.test.ts
@@ -0,0 +1,24 @@
+import { describe, it } from "node:test";
+import { strictEqual } from "node:assert/strict";
+import { runFactory } from "./solution.ts";
+
+const itCases = [
+    { factory: [">>."], expectedOutput: "completed" },
+    { factory: [">>>"], expectedOutput: "broken" },
+    { factory: [">><"], expectedOutput: "loop" },
+    { factory: [">>v", "..<"], expectedOutput: "completed" },
+    { factory: [">>v", "<<<"], expectedOutput: "broken" },
+    { factory: [">v.", "^.."], expectedOutput: "completed" },
+    { factory: [">v.", "^.."], expectedOutput: "loop" },
+];
+
+describe("runFactory(factory)", () => {
+    itCases.forEach(({ factory, expectedOutput }) => {
+        const result = runFactory(factory);
+        const testFunction = () => strictEqual(result, expectedOutput);
+        it(
+            `runFactory(${factory}) should return >${expectedOutput}<`,
+            testFunction
+        );
+    });
+});
---
+++ day-13-assembly-line/solution.ts
@@ -0,0 +1,7 @@
+type Factory = string[];
+type Result = "completed" | "broken" | "loop";
+
+export const runFactory = (factory: Factory): Result => {
+    // Code here
+    return "completed";
+};
